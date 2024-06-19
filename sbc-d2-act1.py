7c3481ba1836b383d0ca5c16aa19144c032e30df
Jared Burdeos <132859086+therecordbreaker19@users.noreply.github.com>
2024-06-18 21:12:19 -0700

    Add files via upload

---
+++ sbc-d2-act1.py
@@ -0,0 +1,15 @@
+word = "summer bootcamp"
+
+#Summer Bootcamp
+#Summer bootcamP
+#Loot
+#CamE
+#ER
+#True = Alpha
+
+print(word.title())
+print(word.replace("p","p"))
+print(word[7:11].replace("b","L"))
+print(word[11:].replace("p","E"))
+print(word[12].upper() + word[5].upper())
+print(word.replace(" ", "ss").isalpha())

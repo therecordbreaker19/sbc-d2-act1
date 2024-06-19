3f81a4eedffbb963123993668dad4d43d24023b6
Jared Burdeos <132859086+therecordbreaker19@users.noreply.github.com>
2024-06-18 23:11:25 -0700

    sbc-d2-act1.py

--- sbc-d2-act1.py
+++ sbc-d2-act1.py
@@ -10,6 +10,6 @@ word = "summer bootcamp"
 print(word.title())
 print(word.capitalize().replace("p","p"))
 print(word[7:11].replace("b","L"))
-print(word[11:].replace("p","E"))
+print(word[11:15].replace("p","E"))
 print(word[12].upper() + word[5].upper())
 print(word.replace(" ", "ss").isalpha())
